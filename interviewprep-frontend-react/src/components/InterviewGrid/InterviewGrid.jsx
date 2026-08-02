41764325f8e8ef3573a96ad72f5f51acd15a2a04
arshmattu08 <singharshpreet6060@gmail.com>
2026-08-01 22:21:29 -0400

    added border on speaker

--- interviewprep-frontend-react/src/components/InterviewGrid/InterviewGrid.jsx
+++ interviewprep-frontend-react/src/components/InterviewGrid/InterviewGrid.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./InterviewGrid.css";
-import avatar from "../../assets/avatar2.jpeg"
+import avatar from "../../assets/avatar2.jpeg";
+import { AppContext } from "../App";
+import { useContext } from "react";
 
-const Grid = () => {
+const Grid = ({whoTalking}) => {
+
+    const {currentUser} = useContext(AppContext)
 
     return (
                 <div id = {"grid-wrapper"}>
 
-                    <div className={"avatar"}>
+                    <div className={whoTalking == "ai" ? "avatar speaking" : "avatar"}>
                         <img className={"avatar-img"} src={avatar}/>
                     </div>
 
-                    <div className={"interviewee"}>
-                        You
+                    <div className={whoTalking == "user" ? "interviewee speaking" : "interviewee"}>
+                        {currentUser.first_name}
                     </div>
 
 
