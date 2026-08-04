fdd29a50231f7c3863749172ea7f14a284a2d453
arshmattu08 <singharshpreet6060@gmail.com>
2026-08-03 23:05:34 -0400

    added edit functionality

--- interviewprep-frontend-react/src/pages/Profile/Profile.jsx
+++ interviewprep-frontend-react/src/pages/Profile/Profile.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Profile.css";
 import HemiSphere from "../../components/HemiSphere/HemiSphere";
 import Footer from "../../components/Footer/Footer";
+import { AppContext } from "../../components/App";
+import { useContext } from "react";
+import Edit from "../../assets/edit.svg"
+import Add from "../../assets/add.svg";
+import Button from "../../components/Button/Button";
 
 const Profile = () => {
+
+    const {currentUser,setCurrentUser,setCreditBoxOpen} = useContext(AppContext)
+
+    const [isEditingFirstName, setEditingFirstName] = useState(false)
+    const [isEditingLastName, setEditingLastName] = useState(false)
+    const [isEditingEmail, setEditingEmail] = useState(false)
+    const[firstNameInput, setFirstNameInput] = useState("")
+    const[lastNameInput, setLastNameInput] = useState("")
+    const[emailInput, setEmailInput] = useState("")
+
+
+    const handleSave = async (field, value, toggle) => {
+        await fetch(" https://impolite-buckle-harddisk.ngrok-free.dev/{our_endpoint}", {
+            method:"PATCH",
+            body: JSON.stringify({[field]: value}) }
+        )
+        setCurrentUser(prev => ({...prev, [field] : value}))
+        toggle(false)
+    }
+
+
+
 
     return (
         <>
@@ -16,28 +43,41 @@ const Profile = () => {
                 <div id="title">Profile Information</div>
 
                 <div className="field">
-                    <h5>First Name</h5>
-                    <input></input>
+                    <h3>First Name</h3>
+                    {isEditingFirstName ? 
+                    <input value={firstNameInput} onChange={(e)=> setFirstNameInput(e.target.value)}></input> :
+                    <span><h3>{currentUser?.first_name}</h3></span>}
+                    {isEditingFirstName && <Button label={"Save"} onClick={() => handleSave(currentUser.first_name, firstNameInput, setEditingFirstName)}/>}
+                    <img src={Edit} onClick={() => setEditingFirstName(!isEditingFirstName)}></img>
                 </div>
 
                 <div className="field">
-                     <h5>Last Name</h5>
-                    <input></input>
+                     <h3>Last Name</h3>
+                    {isEditingLastName ? 
+                    <input value={lastNameInput} onChange={(e)=> setLastNameInput(e.target.value)}></input> :
+                    <span><h3>{currentUser?.last_name}</h3></span>}
+                    {isEditingLastName && <Button label={"Save"} onClick={() => handleSave(currentUser.last_name, lastNameInput, setEditingLastName)}/>}
+                    <img src={Edit} onClick={() => setEditingLastName(!isEditingLastName)}></img>
                 </div>
 
                 <div className="field">
-                     <h5>Email</h5>
-                    <input></input>
+                     <h3>Email</h3>
+                     {isEditingEmail ? 
+                    <input value={emailInput} onChange={(e)=> setEmailInput(e.target.value)}></input> :
+                    <span><h3>{currentUser?.email}</h3></span>}
+                    {isEditingEmail && <Button label={"Save"} onClick={() => handleSave(currentUser.email, emailInput, setEditingEmail)}/>}
+                    <img src={Edit} onClick={()=> setEditingEmail(!isEditingEmail)}></img>
                 </div>
 
-                <div className="field">
-                     <h5>Change Password</h5>
-                    <input></input>
+                <div id="change-pass">
+                    <button>Change Password</button>
                 </div>
 
                 <div id="credit-balance">
                     <h5>Credit Balance:</h5>
-                    <div>0</div>
+                    <div> {currentUser?.credit_balance} </div>
+                     <img src={Add} onClick={() => setCreditBoxOpen(true)}></img>
+
                 </div>
                 <div id="delete-account">
                     <p>Want to delete account?</p>
